7b01e8689a83cd2ee413ff23a5542f485a801a1f
ralphie <ralpheiligan@gmail.com>
2023-01-21 03:09:36 -0500

    card ver 1

---
+++ app/joblist/page.jsx
@@ -0,0 +1,12 @@
+import JobCard from "@/components/job/jobcard";
+
+export default function JobList() {
+  return (
+    <>
+      <JobCard />
+      <JobCard />
+      <JobCard />
+      <JobCard />
+    </>
+  );
+}
