6d2c2224c17e7f2826211e0c332f1232a1815c09
ralphie <ralpheiligan@gmail.com>
2023-01-22 15:15:07 -0500

    navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,44 +6,66 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head />
       <body>
-        <div className="navbar bg-base-100">
-          <div className="flex-1">
-            <Link href="/" className="normal-case text-xl ">
-              ēzēir
-            </Link>
-          </div>
-          <div className="flex-none gap-2">
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="Search"
-                className="input input-bordered input-sm"
-              />
-            </div>
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img src="https://placeimg.com/80/80/people" />
-                </div>
+        <div className="navbar bg-base-100 shadow-lg">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
               </label>
               <ul
                 tabIndex={0}
-                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               >
                 <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
+                  <a>Dispatch</a>
+                </li>
+                <li tabIndex={0}>
+                  <a className="justify-between">Delivery</a>
                 </li>
                 <li>
-                  <a>Settings</a>
-                </li>
-                <li>
-                  <a>Logout</a>
+                  <a>Jobs</a>
                 </li>
               </ul>
             </div>
+            <a
+              href="/"
+              className="normal-case text-2xl font-bold text-secondary  md:m-auto"
+            >
+              ēzēir
+            </a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <a>Dispatch</a>
+              </li>
+              <li tabIndex={0}>
+                <a>Delivery</a>
+              </li>
+              <li>
+                <a>Jobs</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <input
+              type="text"
+              placeholder="Search...."
+              className="input w-full md:w-auto max-w-xs input-sm bg-base-200"
+            />
           </div>
         </div>
         {children}
