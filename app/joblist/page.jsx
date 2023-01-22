48252aa2a23889bcf9fc1ed101746d2b4b3928f8
ralphie <ralpheiligan@gmail.com>
2023-01-21 19:02:26 -0500

    sample API fetch

--- app/joblist/page.jsx
+++ app/joblist/page.jsx
@@ -1,9 +1,22 @@
 import JobCard from "@/components/job/jobcard";
 
-export default function JobList() {
+async function getData() {
+  let res = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+  console.log(res.json);
+  return res.json();
+}
+
+export default async function JobList() {
+  const table = await getData();
   return (
     <>
       <JobCard />
+      <ul>
+        {table.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
